0695de37c23d073937a493bf907a4482c25624f2
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-01-23 04:17:32 +0000

    File changes

--- src/components/factfind/FactFindLayout.jsx
+++ src/components/factfind/FactFindLayout.jsx
@@ -146,7 +146,15 @@ export default function FactFindLayout({ children, currentSection, factFind }) {
       </div>
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
+        {!sidebarOpen && (
+          <button
+            onClick={() => setSidebarOpen(true)}
+            className="absolute left-0 top-1/2 -translate-y-1/2 z-10 w-10 h-10 rounded-r-lg bg-slate-800 text-white flex items-center justify-center hover:bg-slate-700 transition-all"
+          >
+            <ChevronRight className="w-5 h-5" />
+          </button>
+        )}
         {children}
       </div>
     </div>
