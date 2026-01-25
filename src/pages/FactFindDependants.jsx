5819e3b17d02e30b66ac4b0462f81b7ddd829e73
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-01-25 06:19:49 +0000

    File changes

--- src/pages/FactFindDependants.jsx
+++ src/pages/FactFindDependants.jsx
@@ -486,8 +486,7 @@ export default function FactFindDependants() {
                         onClick={handleAddChild}
                         className="bg-blue-600 hover:bg-blue-700 text-white"
                       >
-                        <Plus className="w-4 h-4 mr-2" />
-                        {selectedChildIndex !== null ? 'Update Child' : 'Add Child'}
+                        Save Changes
                       </Button>
                     </div>
                   </CardContent>
