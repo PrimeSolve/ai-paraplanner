f14b6b7292e17bafd54a7a3bd4216755e368309e
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-01-27 05:33:09 +0000

    File changes

--- src/pages/AdviceGroupCompleted.jsx
+++ src/pages/AdviceGroupCompleted.jsx
@@ -147,7 +147,7 @@ export default function AdviceGroupCompleted() {
 
   if (loading) {
     return (
-      <AdviceGroupLayout currentPage="completed">
+      <AdviceGroupLayout currentPage="AdviceGroupCompleted">
         <div className="flex items-center justify-center min-h-screen">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
         </div>
