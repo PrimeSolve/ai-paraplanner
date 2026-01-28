02422a6dca821c8c2adfb8593934a0b4df339143
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-01-28 04:38:30 +0000

    File changes

--- src/pages/AdviceGroupDashboard.jsx
+++ src/pages/AdviceGroupDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { base44 } from '@/api/base44Client';
+import { useRole } from '../components/RoleContext';
 import { 
   FileText, 
   Clock, 
