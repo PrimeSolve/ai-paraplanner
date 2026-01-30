9935a11e17c8fdff328981760629c78231462b2f
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-01-30 05:45:55 +0000

    File changes

--- src/components/soa/SOARequestLayout.jsx
+++ src/components/soa/SOARequestLayout.jsx
@@ -203,12 +203,7 @@ export default function SOARequestLayout({ children, currentSection, soaRequest 
               e.currentTarget.style.transform = 'translateY(0)';
             }}
           >
-            <span style={{ display: 'inline-flex', alignItems: 'center', gap: '8px' }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M19 12H5M12 19l-7-7 7-7"/>
-              </svg>
-              Save & Close
-            </span>
+            Save & Close
           </button>
         </div>
       </div>
