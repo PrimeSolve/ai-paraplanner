1da94171a9606eb39937311a155bd3778421d680
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-02-01 01:26:43 +0000

    File changes

--- src/components/soa/SOARequestLayout.jsx
+++ src/components/soa/SOARequestLayout.jsx
@@ -209,7 +209,7 @@ export default function SOARequestLayout({ children, currentSection, soaRequest 
       </div>
 
       {/* Main Content Area */}
-      <div className="absolute left-80 right-0 top-0 bottom-0 overflow-auto">
+      <div className="absolute left-80 right-0 top-16 bottom-0 overflow-auto">
         {children}
       </div>
     </div>
