371986f731f9b65c3ae15ab7625ed50214f86f10
PrimeSolve Group <tim.hall@primesolve.com.au>
2026-02-04 12:53:41 +0000

    File changes

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -145,15 +145,16 @@ export default function AppShell({ children, pageActions, pageTitle }) {
   }
 
   console.log('AppShell rendering');
-  console.log('isSpecialLayout:', isSpecialLayout);
-  console.log('location.pathname:', location.pathname);
-  console.log('currentLevel:', currentLevel);
-  console.log('originalRole:', originalRole);
+   console.log('isFactFindOrSOA:', isFactFindOrSOA);
+   console.log('isClientPortal:', isClientPortal);
+   console.log('location.pathname:', location.pathname);
+   console.log('currentLevel:', currentLevel);
+   console.log('originalRole:', originalRole);
 
-  return (
-     <div className="flex min-h-screen bg-[#f8fafc]">
-       {renderSidebar()}
-       <div style={{ flex: 1, display: 'flex', flexDirection: 'column', paddingTop: isSpecialLayout ? '0' : '64px', marginLeft: isSpecialLayout ? '0' : '260px' }}>
+   return (
+      <div className="flex min-h-screen bg-[#f8fafc]">
+        {renderSidebar()}
+        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', paddingTop: (isFactFindOrSOA || isClientPortal) ? '0' : '64px', marginLeft: getContentMargin() }}>
          <AppHeader pageActions={pageActions} pageTitle={pageTitle} />
          <main className="flex-1">
            {children}
